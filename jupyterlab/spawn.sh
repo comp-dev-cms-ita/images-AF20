b1ff4dc90b22df467c1704f9c26c5a6327ef88a7
Tommaso Tedeschi <tedesk95@gmail.com>
2025-03-05 12:36:31 +0100

    Update IAM endpoint

--- jupyterlab/spawn.sh
+++ jupyterlab/spawn.sh
@@ -5,7 +5,7 @@ curl -d grant_type=urn:ietf:params:oauth:grant-type:token-exchange \
    -d audience="https://wlcg.cern.ch/jwt/v1/any" \
    -d subject_token=$ACCESS_TOKEN \
    -d scope="openid profile wlcg wlcg.groups" \
-   https://cms-auth.web.cern.ch/token \
+   https://cms-auth.cern.ch/token \
    | tee /tmp/response | jq .access_token |  tr -d '"' |  tr -d '\n'> /tmp/token
 
 while true; do
@@ -14,7 +14,7 @@ while true; do
         -d audience="https://wlcg.cern.ch/jwt/v1/any" \
         -d subject_token=`cat /tmp/token` \
         -d scope="openid profile wlcg wlcg.groups" \
-        https://cms-auth.web.cern.ch/token \
+        https://cms-auth.cern.ch/token \
         | tee /tmp/response | jq .access_token |  tr -d '"' |  tr -d '\n'> /tmp/token_tmp \
     && cp /tmp/token_tmp /tmp/token
     sleep 72000
